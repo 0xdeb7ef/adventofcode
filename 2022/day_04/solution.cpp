5d1f9b6a8e7beea71cd6d1d7a0b280adcf30a83e
0xdeb7ef <45214659+0xdeb7ef@users.noreply.github.com>
2022-12-04 01:08:03 -0500

    a better solution

--- 2022/day_04/solution.cpp
+++ 2022/day_04/solution.cpp
@@ -1,7 +1,5 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -18,32 +16,13 @@ int main(int argc, char const *argv[])
         int r1e = stoi(range1.substr(range1.find('-') + 1, range1.size()));
         int r2s = stoi(range2.substr(0, range2.find('-')));
         int r2e = stoi(range2.substr(range2.find('-') + 1, range2.size()));
-        vector<int> first;
-        vector<int> second;
-        for (int i = r1s; i <= r1e; i++)
-            first.push_back(i);
-        for (int i = r2s; i <= r2e; i++)
-            second.push_back(i);
 
-        // part 1
-        if (includes(first.begin(), first.end(), second.begin(), second.end()) ||
-            includes(second.begin(), second.end(), first.begin(), first.end()))
+        if (((r1s >= r2s) && (r1e <= r2e)) || ((r2s >= r1s) && (r2e <= r1e)))
             count++;
 
-        // part 2
-        int size = first.size() <= second.size() ? first.size() : second.size();
-        vector<int> intersection(size);
-        set_intersection(first.begin(), first.end(),
-                         second.begin(), second.end(),
-                         intersection.begin());
-        for (auto x : intersection)
-        {
-            if (x != 0)
-            {
-                count2++;
-                break;
-            }
-        }
+        if (((r1s >= r2s) && (r1e <= r2e)) || ((r2s >= r1s) && (r2e <= r1e)) ||
+            ((r1e >= r2s) && (r1e <= r2e)) || ((r1s >= r2s) && (r1s <= r2e)))
+            count2++;
     }
 
     cout << count << endl;
