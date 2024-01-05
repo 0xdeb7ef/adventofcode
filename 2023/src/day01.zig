898afb84a814f591dddf7ac31e8e47c28b2d9324
0xdeb7ef <45214659+0xdeb7ef@users.noreply.github.com>
2024-01-05 12:29:44 -0800

    day 01, part 01 & 02

--- 2023/src/day01.zig
+++ 2023/src/day01.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const LineReader = @import("utils.zig").LineReader;
 const Result = @import("utils.zig").Result;
 
+const digits = [9][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
 pub fn day01(allocator: std.mem.Allocator, reader: *LineReader) anyerror!Result {
     _ = allocator;
     var result: Result = std.mem.zeroes(Result);
@@ -9,19 +11,56 @@ pub fn day01(allocator: std.mem.Allocator, reader: *LineReader) anyerror!Result 
     var n: u32 = 0;
 
     while (try reader.next()) |line| : (n += 1) {
-        var first: u8 = 0;
-        var last: u8 = 0;
+        var part2_flag = false;
 
-        for (line) |c| {
-            if (std.ascii.isDigit(c)) {
-                if (first == 0) {
-                    first = c;
+        // forward pass
+        var i: usize = 0;
+        while (i < line.len) : (i += 1) {
+            if (std.ascii.isDigit(line[i])) {
+                result.part1 += try std.fmt.parseInt(u8, &[2]u8{ line[i], '0' }, 10);
+                if (!part2_flag) {
+                    result.part2 += try std.fmt.parseInt(u8, &[2]u8{ line[i], '0' }, 10);
                 }
-                last = c;
+                break;
+            }
+
+            if (part2_flag) continue;
+
+            inline for (digits, 1..) |digit, k| {
+                if (line.len >= i + digit.len) {
+                    if (std.mem.eql(u8, line[i .. i + digit.len], digit)) {
+                        result.part2 += @as(i64, @intCast(k)) * 10;
+                        part2_flag = true;
+                    }
+                }
             }
         }
 
-        result.part1 += try std.fmt.parseInt(u8, &[2]u8{ first, last }, 10);
+        part2_flag = false;
+
+        // backward pass
+        i = line.len;
+        while (i != 0) {
+            i -= 1;
+            if (std.ascii.isDigit(line[i])) {
+                result.part1 += try std.fmt.parseInt(u8, &[1]u8{line[i]}, 10);
+                if (!part2_flag) {
+                    result.part2 += try std.fmt.parseInt(u8, &[1]u8{line[i]}, 10);
+                }
+                break;
+            }
+
+            if (part2_flag) continue;
+
+            inline for (digits, 1..) |digit, k| {
+                if (line.len >= i + digit.len) {
+                    if (std.mem.eql(u8, line[i .. i + digit.len], digit)) {
+                        result.part2 += @intCast(k);
+                        part2_flag = true;
+                    }
+                }
+            }
+        }
     }
 
     return result;
